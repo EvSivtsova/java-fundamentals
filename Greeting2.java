76285e03a8dd4a71de752f615076e8147820cd34
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-10-31 16:39:50 +0000

    Add Greeting2 - prints greeting with age & name

---
+++ Greeting2.java
@@ -0,0 +1,9 @@
+public class Greeting2 {
+    public static void main(String[] artgs) {
+        String name = "Curtis";
+        String greeting = "Hello";
+        Integer age = 23;
+
+        System.out.printf("%s %s, you are %d years old \n", greeting, name, age);
+    }
+}
