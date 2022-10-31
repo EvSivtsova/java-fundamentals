54b949ff8e1496886a869ed66f494594832321ae
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-10-31 16:20:31 +0000

    Add Greeting class - uses name variable

---
+++ Greeting.java
@@ -0,0 +1,11 @@
+public class Greeting {
+    public static void main(String[] arg) {
+        String name = "Anna";
+        String greeting = "Hello";
+
+        String message = greeting + " " + name;
+
+        System.out.println(message);
+
+    }
+}
