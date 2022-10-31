1eba0be726da54b9086db70c7e05c92b144d0391
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-10-31 18:51:50 +0000

    Add BetterCalulators - adds up non whole numbers

---
+++ types-and-compilation/BetterCalculator.java
@@ -0,0 +1,8 @@
+// package types-and-compilation;
+
+public class BetterCalculator {
+    public static void main(String[] args) {
+        Double result = 2.5 + 2.5;
+        System.out.printf("The result is %,.2f \n", result);
+    }
+}
