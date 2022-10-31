ab73fb89b7ec5498235c641a11126d1570bdaec1
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-10-31 18:58:05 +0000

    Add PriceDisplay - prints item's price as a string

---
+++ types-and-compilation/PriceDisplay.java
@@ -0,0 +1,10 @@
+// package types-and-compilation;
+
+public class PriceDisplay {
+    public static void main(String[] args) {
+        String itemName = "Coffee Machine";
+        Double price = 15.99;
+
+        System.out.printf("The price for the item %s is %,.2f \n", itemName, price);
+    }
+}
