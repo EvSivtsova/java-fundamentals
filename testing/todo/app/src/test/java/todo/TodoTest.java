8532743b42a555ad3dc046c22604701b1e2aac5b
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-11-01 11:29:32 +0000

    Rename tests for Todo

--- testing/todo/app/src/test/java/todo/TodoTest.java
+++ testing/todo/app/src/test/java/todo/TodoTest.java
@@ -6,12 +6,12 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 
 public class TodoTest {
-    @Test public void startsWithEmptyList() {
+    @Test public void shouldStartsWithEmptyList() {
         Todo todo = new Todo();
         assertTrue("initialises with an empty list of tasks", todo.tasks.isEmpty());
     }
 
-    @Test public void addsTasks() {
+    @Test public void shouldAddTasks() {
         Todo todo = new Todo();
         ArrayList<String> expectedTasks = new ArrayList<String>();
         expectedTasks.add("Walk the dog");
@@ -19,7 +19,7 @@ public class TodoTest {
         assertEquals("todo list contains added task", expectedTasks, todo.addTask("Walk the dog"));
     }
 
-    @Test public void getsTasks() {
+    @Test public void shouldGetTasks() {
         Todo todo = new Todo();
         ArrayList<String> expectedTasks = new ArrayList<String>();
         expectedTasks.add("Walk the dog");
