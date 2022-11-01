84f535f890abd94313f369a04d66a677da5639b7
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-11-01 08:20:41 +0000

    Add Calculator tests and class - TDDd

---
+++ testing/calculator/app/src/main/java/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+    }
+
+    Integer add(Integer integer1, Integer integer2) {
+        return integer1 + integer2;
+    }
+}
