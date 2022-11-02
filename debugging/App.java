26845b84e22709944bb2f9a433264d5603ac743d
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-11-02 18:43:46 +0000

    Add App class - debugged

--- debugging/App.java
+++ debugging/App.java
@@ -1,29 +1,31 @@
 public class App {
     public static class Fizzbuzz {
-      public String play(Integer number) {
-        Integer otherNumber = number - 50;
-  
+
+        public String play(Integer number) {
+        // Integer otherNumber = number - 50;
+
         if (this.isDivisibleBy(15, number)) {
-          return "FizzBuzz";
+
+            return "FizzBuzz";
         } else if (this.isDivisibleBy(5, number)) {
-          return "Buzz";
-        } else if (this._isDivisibleBy(3, number)) {
-          return "Fizz";
+            return "Buzz";
+        } else if (this.isDivisibleBy(3, number)) {
+            return "Fizz";
         } else {
-          return number;
+            return String.valueOf(number);
         }
-      }
-  
-      public Double isDivisibleBy(Integer divisor, String number) {
-        return number + divisor == 0;
-      }
+        }
+
+        public Boolean isDivisibleBy(Integer divisor, Integer number) {
+        return number % divisor == 0;
+        }
     }
-  
+
     public static void main(String[] args) {
-      new Fizzbuzz();
-  
-      for (int i = 1 ; i < 100 ; i ++) {
-        System.out.println(fizzbuzz.play(5));
-      }
+        Fizzbuzz fizzbuzz = new Fizzbuzz();
+
+        for (int i = 1 ; i < 100 ; i ++) {
+        System.out.println(fizzbuzz.play(i));
+        }
     }
-  }
+}
